4119e386ff9f460ab93327057a4b2d59bd16400d
Cowleyfitcode <65091695+Mcowley1@users.noreply.github.com>
2020-12-10 17:58:17 -0500

    updated files

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -1,5 +1,3 @@
-const { response } = require("express");
-
 const $backBtn = document.querySelector('#back-btn');
 const $pizzaName = document.querySelector('#pizza-name');
 const $createdBy = document.querySelector('#created-by');
@@ -19,16 +17,18 @@ function getPizza() {
   // get pizzaInfo
   fetch(`/api/pizzas/${pizzaId}`)
     .then(response => {
-      // check for a 4xx or 5xx error from server
+      console.log(response);
       if (!response.ok) {
+        console.log('hi');
         throw new Error({ message: 'Something went wrong!' });
       }
+
       return response.json();
     })
     .then(printPizza)
     .catch(err => {
       console.log(err);
-      alert('Can not find a pizza with this id! Taking you back.');
+      alert('Cannot find a pizza with this id! Taking you back.');
       window.history.back();
     });
 }
@@ -128,13 +128,12 @@ function handleNewCommentSubmit(event) {
     })
     .then(commentResponse => {
       console.log(commentResponse);
-      location.reload();
+      // location.reload();
     })
     .catch(err => {
       console.log(err);
     });
 }
-
 
 function handleNewReplySubmit(event) {
   event.preventDefault();
@@ -180,7 +179,6 @@ function handleNewReplySubmit(event) {
 $backBtn.addEventListener('click', function() {
   window.history.back();
 });
-
 
 $newCommentForm.addEventListener('submit', handleNewCommentSubmit);
 $commentSection.addEventListener('submit', handleNewReplySubmit);
